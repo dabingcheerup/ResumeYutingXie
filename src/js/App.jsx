5e454510eb55028ba22df1eeeb1522a506fd89cb
dabingcheerup <yuting_xie@student.uml.edu>
2018-03-08 21:09:59 -0500

    remove link tags of bootstrap

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -1,9 +1,9 @@
-/* App.jsx
+// App.jsx
 import React from "react";
 import ReactDOM from "react-dom";
 import { Button } from "react-bootstrap";
 
-export default class App extends React.Component {
+/*export default class App extends React.Component {
   render() {
     return <button> Hello React!</button>;
   }
