473300ec059bb1684d94c5957e80f826454dbf64
dabingcheerup <yuting_xie@student.uml.edu>
2018-03-09 22:25:37 -0500

    pass react-fontawsome test

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -2,17 +2,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Button } from "react-bootstrap";
+import FontAwesomeIcon from "@fortawesome/react-fontawesome";
 
 export default class App extends React.Component {
   render() {
     return (
-      <FontAwesome
-        className="super-crazy-colors"
-        name="rocket"
-        size="2x"
-        spin
-        sytlestyle={{ textShadow: "0 1px 0 rgba(0, 0, 0, 0.1)" }}
-      />
+      <div>
+        <FontAwesomeIcon icon="check-square" />
+        Favorite beverage: <FontAwesomeIcon icon="coffee" />
+      </div>
     );
   }
 }
